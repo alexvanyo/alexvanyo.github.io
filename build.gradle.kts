08b1b483db796e508d603bb3586ead9af6f2da00
Alex Vanyo <vanyoalex@gmail.com>
2021-05-18 20:12:13 -0500

    Update compose to dev12

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.4.32"
-    id("org.jetbrains.compose") version "0.0.0-web-dev-11"
+    id("org.jetbrains.compose") version "0.0.0-web-dev-12"
 }
 
 repositories {
