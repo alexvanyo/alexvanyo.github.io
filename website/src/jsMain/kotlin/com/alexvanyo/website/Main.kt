3baf69cd6aa6730625effe0b415cffa3b502239c
Alex Vanyo <vanyo@google.com>
2021-08-14 17:43:17 -0500

    Replace article fetching with produceState

--- website/src/jsMain/kotlin/com/alexvanyo/website/Main.kt
+++ website/src/jsMain/kotlin/com/alexvanyo/website/Main.kt
@@ -1,10 +1,7 @@
 package com.alexvanyo.website
 
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.produceState
 import com.alexvanyo.website.data.websiteJson
 import com.alexvanyo.website.models.Article
 import com.alexvanyo.website.pages.BackstackPage
@@ -19,11 +16,9 @@ import org.w3c.dom.url.URLSearchParams
 
 fun main() {
     renderComposable(rootElementId = "root") {
-        var articles by remember { mutableStateOf<List<Article>>(emptyList()) }
-
-        LaunchedEffect(Unit) {
+        val articles by produceState(emptyList<Article>()) {
             val articlesJson = window.fetch("data/articles.json").await().text().await()
-            articles = websiteJson.decodeFromString(ListSerializer(Article.serializer()), articlesJson)
+            value = websiteJson.decodeFromString(ListSerializer(Article.serializer()), articlesJson)
         }
 
         Style(MainStylesheet)
