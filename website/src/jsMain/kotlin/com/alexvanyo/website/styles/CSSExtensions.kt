73b46ac4b496ee319b7a2dd71b9ca0e72e51714d
Alex Vanyo <vanyo@google.com>
2023-04-07 11:38:58 -0700

    Use new attributes

--- website/src/jsMain/kotlin/com/alexvanyo/website/styles/CSSExtensions.kt
+++ website/src/jsMain/kotlin/com/alexvanyo/website/styles/CSSExtensions.kt
@@ -1,16 +1,6 @@
 package com.alexvanyo.website.styles
 
-import org.jetbrains.compose.web.css.CSSBuilder
 import org.jetbrains.compose.web.css.CSSStyleRuleBuilder
-import org.jetbrains.compose.web.css.StyleScope
-
-fun CSSBuilder.fontFamily(value: String) {
-    property("font-family", value)
-}
-
-fun CSSStyleRuleBuilder.fontFamily(value: String) {
-    property("font-family", value)
-}
 
 fun CSSStyleRuleBuilder.fontVariant(fontVariantCaps: FontVariantCaps) {
     property("font-variant", fontVariantCaps.value)
@@ -24,17 +14,4 @@ enum class FontVariantCaps(val value: String) {
     AllPetiteCaps("all-petite-caps"),
     Unicase("unicase"),
     TitlingCaps("titling-caps"),
-}
-
-fun StyleScope.textAlign(textAlign: TextAlign) {
-    property("text-align", textAlign.value)
-}
-
-enum class TextAlign(val value: String) {
-    Left("left"),
-    Right("right"),
-    Center("center"),
-    Justify("justify"),
-    Initial("initial"),
-    Inherit("inherit"),
 }
