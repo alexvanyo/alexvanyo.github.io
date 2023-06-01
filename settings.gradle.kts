189f60ac3904956ae021715b25d0e714946e3ea6
Alex Vanyo <vanyo@google.com>
2023-05-31 20:31:24 -0700

    Update Compose compiler

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
+        google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
