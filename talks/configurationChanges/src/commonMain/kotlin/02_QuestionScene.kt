e61b5f754682ee59fb9aef4e3cbc0a022bbaa722
Alex Vanyo <vanyo@google.com>
2025-06-14 20:56:36 -0700

    Configuration changes talk WIP

--- talks/configurationChanges/src/commonMain/kotlin/02_QuestionScene.kt
+++ talks/configurationChanges/src/commonMain/kotlin/02_QuestionScene.kt
@@ -1,4 +1,14 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.ExperimentalTransitionApi
+import androidx.compose.animation.core.createChildTransition
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -9,18 +19,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import dev.bnorm.storyboard.StoryboardBuilder
+import dev.bnorm.storyboard.compareTo
+import dev.bnorm.storyboard.easel.assist.SceneCaption
 import dev.bnorm.storyboard.easel.template.SceneEnter
 import dev.bnorm.storyboard.easel.template.SceneExit
+import dev.bnorm.storyboard.toState
 import theme.jetBrainsMono
 
+@OptIn(ExperimentalTransitionApi::class)
 fun StoryboardBuilder.Scene02_QuestionScene() {
     scene(
+        stateCount = 3,
         enterTransition = SceneEnter(Alignment.CenterEnd),
         exitTransition = SceneExit(Alignment.CenterEnd),
     ) {
+        val state = transition.createChildTransition { it.toState() }
+
         Column(
             modifier = Modifier.padding(32.dp).fillMaxSize(),
         ) {
@@ -34,8 +52,34 @@ fun StoryboardBuilder.Scene02_QuestionScene() {
                 },
                 style = MaterialTheme.typography.h3,
             )
-            Spacer(Modifier.height(32.dp))
-            Text("This talk is about that, but be patient")
+            Spacer(Modifier.height(64.dp))
+
+            state.AnimatedVisibility(
+                visible = { it >= 1 },
+                enter = fadeIn() + expandVertically(),
+                exit = fadeOut() + shrinkVertically(),
+            ) {
+                Row {
+                    Text(
+                        "Answer:",
+                        style = MaterialTheme.typography.h3,
+                    )
+                    state.AnimatedVisibility(
+                        visible = { it >= 2 },
+                        enter = fadeIn() + expandHorizontally(),
+                        exit = fadeOut() + shrinkHorizontally(),
+                    ) {
+                        Text(
+                            buildAnnotatedString {
+                                withStyle(SpanStyle(fontStyle = FontStyle.Italic)) {
+                                    append(" stay tuned ")
+                                }
+                            },
+                            style = MaterialTheme.typography.h3,
+                        )
+                    }
+                }
+            }
         }
     }
 }
